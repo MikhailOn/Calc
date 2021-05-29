632c2c586930a6fab6fb53e8c447d4254c7eada4
MikhailOn <sowidanravage@gmail.com>
2021-05-29 05:02:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,58 @@
-import telebot;
+
+import telebot
 bot = telebot.TeleBot('1888931316:AAGpqnyk1bE_Wh-ksj8W1eiGMxPpdeMm0ag');
 
 #Пытаемся сделать, чтобы бот понимал голос, импортируем библиотеки
-
 import requests
 #для обработки HTTP запросов
-import speech_recognition
+import webbrowser
+#для открытия ссылок
+import speech_recognition as sr
 #распознование голоса
 import os
-#для работы с файлами системы
+#для работы с файловой системой
 import pydub
 #для обработки oga файлов (так как тг использует oga, а не wav)
+
 from pydub import AudioSegment
-AudioSegment.converter = os.getcwd() + "C:\Users\crzho\Desktop\ffmpeg-4.0.2-win32-static\bin\ffmpeg.exe"
-AudioSegment.ffmpeg = os.getcwd() + "C:\Users\crzho\Desktop\ffmpeg-4.0.2-win32-static\bin\ffmpeg.exe"
-AudioSegment.ffprobe = os.getcwd() + "C:\Users\crzho\Desktop\ffmpeg-4.0.2-win32-static\bin\ffmpeg.exe"
+AudioSegment.converter = os.getcwd() + "C:\\Users\\crzho\\Desktop\\CALCULATOR\\ffmpeg\\ffmpeg\\bin\\ffmpeg.exe"
+AudioSegment.ffmpeg = os.getcwd() + "C:\\Users\\crzho\\Desktop\\CALCULATOR\\ffmpeg\\ffmpeg\\bin\\ffmpeg.exe"
+AudioSegment.ffprobe = os.getcwd() + "C:\\Users\\crzho\\Desktop\\CALCULATOR\\ffmpeg\\ffmpeg\\bin\\ffprobe.exe"
 #скачал ffmpeg для конвертации oga в wav, так как pyttsx3 понимает только wav
+#однако, выдаёт ошибку RuntimeWarning: Couldn't find ffmpeg or avconv - defaulting to ffmpeg, but may not work warn("Couldn't find ffmpeg or avconv - defaulting to ffmpeg, but may not work", RuntimeWarning)
+#перерыл кучу форумов по этому вопросу - ничего не помогло
 
-## import pyttsx3
-#для отправления на сервера тг голосовых файлов
-## tts = pyttsx3
-## voices = tts.getProperty('voices')
+import pyttsx3
+#cинтез речи, пишем код, чтобы голосовой помощник перекидывал нас на сайт компании
+start = pyttsx3.init()
 
-#Пытаюсь выбрать голос, но почему-то не выдаёт список голосов, не очень понимаю как это сделать
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("скажи что-нибудь")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+        try:
+            task = r.recognize_google(audio, language="ru-RU").lower()
+            print(task)
+        except:
+            task = listen()
+        return task
 
+def request(task):
+    if "сайт" in task:
+        text = "открываю сайт нашей компании"
+        start.say(text)
+        start.runAndWait()
+        webbrowser.open('https://ingos.ru', new=2)
+
+while True:
+    request(listen())
 
 #Приветствие бота
-@bot.message_handler(content_types=['text'])
+import telebot
+@bot.message_handler (content_types=['text'])
 def get_text_messages(message):
     if message.text == "Здравствуйте":
         bot.send_message(message.from_user.id, "Здравствуйте! Я страховой консультант Мила, и сейчас я помогу разобраться Вам с любым интересующим Вас вопросом!")
@@ -34,4 +60,7 @@ def get_text_messages(message):
             bot.send_message(message.from_user.id, "Чтобы задать вопрос боту, сначала поприветствуйте его словом Здравствуйте")
     else:
                 bot.send_message(message.from_user.id, "Я Вас не поняла. Напишите /help.")
-bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0);
+
+
+#результат - голосовой помощник перекидывает на сайт, но только, если запускать программу с компа - как интегрировать это все в телеграмм пока непонятно.
